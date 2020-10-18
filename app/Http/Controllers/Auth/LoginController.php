de11d6fedf16f030d942a305bc2c512c91f1dc65
JAYDITCH BALANSI <2164482@slu.edu.ph>
2020-10-18 21:21:54 +0800

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
+use App\User;
+use Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('facebook')->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $fbuser = Socialite::driver('facebook')->user();
+
+        $finduser = User::where('email', $fbuser->email)->first();
+        if($finduser){
+            Auth::login($finduser);
+            return redirect()->route('home');
+        }else{
+            $createUser = new User;
+            $createUser->name = $fbuser->name;
+            $createUser->email = $fbuser->email;
+         $createUser->password = bcrypt('123456789');
+            $createUser->save();
+            Auth::login($createUser);
+            return redirect()->route('home');
+        }
+    }
+}
